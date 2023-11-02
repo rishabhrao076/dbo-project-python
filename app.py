0d932255258b23b86eb2c8ef4853b59af1766f79
Rishabh Rao <rishabhrao076@gmail.com>
2023-11-02 15:34:31 -0500

    added postgres connection and environment file

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
+
+# Configure the PostgreSQL database URI using environment variables
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_DATABASE')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize SQLAlchemy
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     # Data to be passed to the template
     name = "Flask User"
     # Render the template and pass data to it
-    return render_template('index.html', name=name)
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
