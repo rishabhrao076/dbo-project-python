070c5bdde77fe60716f89dcd31320e0744dfd8d2
Rishabh Rao <rishabhrao076@gmail.com>
2023-11-03 18:38:58 -0500

    nav bar update and tv shows and movies added

--- app.py
+++ app.py
@@ -157,6 +157,33 @@ def dashboard():
     
     return render_template('dashboard.html',items=items)
 
+@app.route('/movies')
+@login_required
+def movies():
+    query = db.sql.text('''select * from ( select a.*, row_number() over (partition by a.genre_id order by a.genre_id,a.added_on desc) as rn from ( select distinct on (contents.content_id) contents.*, genre.genre, genre.genre_id from contents join content_genre on content_genre.content_id = contents.content_id join genre on genre.genre_id = content_genre.genre_id where contents.content_type = 1) as a order by genre_id ) as b where rn <= 20''')
+
+    items = db.session.execute(query).fetchall()
+    
+    items = [r._asdict() for r in items]
+        
+    items = group_by_key(items,'genre')
+    
+    return render_template('dashboard.html',items=items)
+
+@app.route('/tvshows')
+@login_required
+def tvShows():
+    query = db.sql.text('''select * from ( select a.*, row_number() over (partition by a.genre_id order by a.genre_id,a.added_on desc) as rn from ( select distinct on (contents.content_id) contents.*, genre.genre, genre.genre_id from contents join content_genre on content_genre.content_id = contents.content_id join genre on genre.genre_id = content_genre.genre_id where contents.content_type = 2) as a order by genre_id ) as b where rn <= 20''')
+
+    items = db.session.execute(query).fetchall()
+    
+    items = [r._asdict() for r in items]
+        
+    items = group_by_key(items,'genre')
+    
+    return render_template('dashboard.html',items=items)
+
+
 
 @app.route('/content-metadata')
 @login_required
