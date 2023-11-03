07a016c9e5490e9d36afa9d848171d222b2b790a
Rishabh Rao <rishabhrao076@gmail.com>
2023-11-02 23:42:52 -0500

    dashboard functionality completed

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-from helpers.collectionHelpers import group_by_index
+from helpers.collectionHelpers import group_by_index, group_by_key
 
 # Load environment variables from .env file
 load_dotenv()
@@ -47,13 +47,24 @@ def dashboard():
     query = db.sql.text('''select * from ( select a.*, row_number() over (partition by a.genre_id order by a.genre_id,a.added_on desc) as rn from ( select distinct on (contents.content_id) contents.*, genre.genre, genre.genre_id from contents join content_genre on content_genre.content_id = contents.content_id join genre on genre.genre_id = content_genre.genre_id ) as a order by genre_id ) as b where rn <= 20''')
 
     items = db.session.execute(query).fetchall()
-    print(items[0])
-    items = group_by_index(items,-3)
-    print(items['Comedy'])
-
-
-    # Render the template and pass data to it
+    
+    items = [r._asdict() for r in items]
+        
+    items = group_by_key(items,'genre')
+    
     return render_template('dashboard.html',items=items)
+
+
+@app.route('/content-metadata')
+def contentMetadata():
+
+    query = db.sql.text("With contentlanguagesgenres as ( select contents.*, string_agg(distinct languages.language, ', ') as languages, string_agg(distinct genre.genre, ', ') as genres from contents join media on contents.content_id = media.content_id join content_language on contents.content_id = content_language.content_id join languages on content_language.language_id = languages.language_id join content_genre on content_genre.content_id = contents.content_id join genre on genre.genre_id = content_genre.genre_id where contents.content_id = :content_id group by contents.content_id ) select contentlanguagesgenres.*, media.* from contentlanguagesgenres join media on media.content_id = contentlanguagesgenres.content_id")
+
+    items = db.session.execute(query,{'content_id':request.args.get('contentId')}).fetchall()
+
+    result_dicts = [r._asdict() for r in items]
+
+    return jsonify(result_dicts)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- helpers/collectionHelpers.py
+++ helpers/collectionHelpers.py
@@ -6,3 +6,11 @@ def group_by_index(data, index):
             grouped_data[key] = []
         grouped_data[key].append(item)
     return grouped_data
+def group_by_key(data,key):
+    grouped_data = {}
+    for item in data:
+        item_key = item.get(key)
+        if item_key not in grouped_data:
+            grouped_data[item_key] = []
+        grouped_data[item_key].append(item)
+    return grouped_data
